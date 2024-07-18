8570cf9021bc1d25916a75454b987ccf63ad3055
SREEHARI P V <sreehariv07@gmail.com>
2024-07-18 20:23:19 +0530

    updated api file

--- app/api/cabins/[cabinId]/route.ts
+++ app/api/cabins/[cabinId]/route.ts
@@ -1,22 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
 import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
 
-interface GETProps {
-  params: {
-    cabinId: string | number;
-  };
-}
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const { searchParams } = new URL(request.url);
+  const cabinId = searchParams.get("cabinId");
 
-//The function names should follow the HTTP verb name conventions (GET,POST,PUT)
-export async function GET({ params }: GETProps): Promise<Response> {
-  const { cabinId } = params;
+  if (!cabinId) {
+    return NextResponse.json(
+      { message: "Cabin ID is required" },
+      { status: 400 }
+    );
+  }
 
   try {
     const [cabin, bookedDates] = await Promise.all([
       getCabin(cabinId),
       getBookedDatesByCabinId(cabinId),
     ]);
-    return Response.json({ cabin, bookedDates });
+    return NextResponse.json({ cabin, bookedDates });
   } catch (error) {
-    return Response.json({ message: "Cabin is not found" });
+    return NextResponse.json(
+      { message: "Cabin is not found" },
+      { status: 404 }
+    );
   }
 }
